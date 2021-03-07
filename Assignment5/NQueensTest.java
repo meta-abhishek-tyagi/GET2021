360655f08f31aa12177d6e9ea0c025b840902732
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-08 02:46:53 +0530

    Create NQueensTest.java

---
+++ Assignment5/NQueensTest.java
@@ -0,0 +1,13 @@
+package Assignment5;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NQueensTest {
+	boolean output=true;
+	NQueens obj=new NQueens();
+	@Test
+	public void NQueensTestCase(){
+		assertEquals(output, obj.solveNQ());
+	}
+}
