03df9a37bdc0e39c3b451203eaec2e4347351c47
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-08 00:09:10 +0530

    Create LCM_HCF_Test.java

---
+++ Assignment5/LCM_HCF_Test.java
@@ -0,0 +1,23 @@
+package Assignment5;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LCM_HCF_Test {
+	int x=5;
+	int y=10;
+	int lcm=10;
+	int hcf=5;
+	
+	//Test case for LCM
+	@Test
+	public void LCM_TestCase(){
+		assertEquals(lcm, LCM_HCF.lcm(x, y));
+	}
+	
+	//Test case for HCF
+	@Test
+	public void HCF_TestCase(){
+		assertEquals(hcf, LCM_HCF.hcf(x, y));
+	}
+}
