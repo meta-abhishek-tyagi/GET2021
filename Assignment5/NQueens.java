15d8ba2c52ba19aa3a3c92d93f5898e5669f020a
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-08 11:17:45 +0530

    Update NQueens.java

--- Assignment5/NQueens.java
+++ Assignment5/NQueens.java
@@ -2,9 +2,8 @@ package Assignment5;
 class NQueens{ 
 	final int N = 4; 
 
-	/* function to print solution */
-	void printSolution(int board[][]) 
-	{ 
+	// function to print solution 
+	void printSolution(int board[][]) { 
 		for (int i = 0; i < N; i++) { 
 			for (int j = 0; j < N; j++) 
 				System.out.print(" " + board[i][j] + " "); 
@@ -12,18 +11,17 @@ class NQueens{
 		} 
 	} 
 
-	// function to check if a queen can be placed on board.
-	boolean nQueen(int board[][], int row, int col) 
-	{ 
+	// function to check if a queen can be placed on board or not.
+	boolean nQueen(int board[][], int row, int col){ 
 		int i, j; 
-
+		
 		// Check this row on left side 
-		for (i = 0; i < col; i++) 
+		for (i = 0; i < col; i++)
 			if (board[row][i] == 1) 
 				return false; 
-
+		
 		// Check upper diagonal on left side 
-		for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+		for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
 			if (board[i][j] == 1) 
 				return false; 
 
@@ -31,7 +29,6 @@ class NQueens{
 		for (i = row, j = col; j >= 0 && i < N; i++, j--) 
 			if (board[i][j] == 1) 
 				return false; 
-
 		return true; 
 	} 
 
@@ -48,13 +45,13 @@ class NQueens{
 			if (nQueen(board, i, col)) { 
 				board[i][col] = 1; 
 
-				// place rest of the queens
-				if (solveNQueen(board, col + 1) == true) 
-					return true; 
+			       // place rest of the queens
+			       if (solveNQueen(board, col + 1) == true) 
+				         return true; 
 
-				// If placing queen in board[i][col] doesn't lead to a solution then remove queen from board[i][col]
-				board[i][col] = 0; // BACKTRACK 
-			} 
+			      // If placing queen in board[i][col] doesn't lead to a solution then remove queen from board[i][col]
+		       	      board[i][col] = 0; // BACKTRACK 
+		       } 
 		} 
 
 		// If the queen can not be placed in any row in this column col, then return false 
@@ -65,9 +62,9 @@ class NQueens{
 	boolean solveNQ() 
 	{ 
 		int board[][] = { { 0, 0, 0, 0 }, 
-						  { 0, 0, 0, 0 }, 
-						  { 0, 0, 0, 0 }, 
-						  { 0, 0, 0, 0 } }; 
+				  { 0, 0, 0, 0 }, 
+				  { 0, 0, 0, 0 }, 
+				  { 0, 0, 0, 0 } }; 
 
 		if (solveNQueen(board, 0) == false) { 
 			System.out.print("Solution does not exist"); 
