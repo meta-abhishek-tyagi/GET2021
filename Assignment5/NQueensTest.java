d617b8f191bbd8b0a66f67bc2c29394bcbde509c
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-08 11:18:18 +0530

    Update NQueensTest.java

--- Assignment5/NQueensTest.java
+++ Assignment5/NQueensTest.java
@@ -1,11 +1,12 @@
 package Assignment5;
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class NQueensTest {
 	boolean output=true;
 	NQueens obj=new NQueens();
+	
 	@Test
 	public void NQueensTestCase(){
 		assertEquals(output, obj.solveNQ());
