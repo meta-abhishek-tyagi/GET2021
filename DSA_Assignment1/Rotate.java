f905851a66c90957aac569a50c71e75f00b42f5e
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-11 01:34:21 +0530

    Update Rotate.java

--- DSA_Assignment1/Rotate.java
+++ DSA_Assignment1/Rotate.java
@@ -3,16 +3,16 @@ class Rotate{
 	
      //Method to rotate a subLinkedList	
      public static LinkedList rotateSubList(LinkedList<Integer> subLinkedList, int noOfSteps){	
-	int length=subLinkedList.size();	
+	int length = subLinkedList.size();	
 	if(noOfSteps > length)
 	   noOfSteps = noOfSteps % length;	   
 	noOfSteps = length - noOfSteps;	
-	if(noOfSteps==0 || noOfSteps == length)
+	if(noOfSteps == 0 || noOfSteps == length)
 	   return subLinkedList;
-	int cnt=0;
-	int i=0; 
+	int cnt = 0;
+	int i = 0; 
 	while(cnt < noOfSteps){
-	   int temp=subLinkedList.get(i);
+	   int temp = subLinkedList.get(i);
 	   subLinkedList.remove(i);
 	   subLinkedList.addLast(temp);
 	   cnt++;
@@ -40,27 +40,27 @@ class Rotate{
      }
 	
      public static void main(String args[]){
-	Scanner sc=new Scanner(System.in);
-	LinkedList<Integer> linkedList=new LinkedList<>();
-	LinkedList<Integer> subLinkedList=new LinkedList<>();
-	LinkedList<Integer> rotatedList=new LinkedList<>();	
+	Scanner sc = new Scanner(System.in);
+	LinkedList<Integer> linkedList = new LinkedList<>();
+	LinkedList<Integer> subLinkedList = new LinkedList<>();
+	LinkedList<Integer> rotatedList = new LinkedList<>();	
 	System.out.println("Enter number of elements in linked list : ");
-	int noOfElements=sc.nextInt();	
+	int noOfElements = sc.nextInt();	
 	for(int i=0;i<noOfElements;i++){
 	   System.out.print("Enter Element " + (i+1) + " : ");
 	   linkedList.add(sc.nextInt());
 	}	
 	System.out.println("Enter Left position of sub-list : ");
-	int L=sc.nextInt();	
+	int L = sc.nextInt();	
 	System.out.println("Enter Right position of sub-list : ");
-	int R=sc.nextInt();	
+	int R = sc.nextInt();	
 	for(int i=L-1;i<R;i++){
 	   subLinkedList.add(linkedList.get(i));
 	}		
 	System.out.println("Enter no. of steps to rotate sub-list : ");
-	int noOfSteps=sc.nextInt();	
-	subLinkedList=rotateSubList(subLinkedList, noOfSteps);
-	rotatedList=outputList(subLinkedList, linkedList, L, R);
+	int noOfSteps = sc.nextInt();	
+	subLinkedList = rotateSubList(subLinkedList, noOfSteps);
+	rotatedList = outputList(subLinkedList, linkedList, L, R);
 	printList(rotatedList);
      }
 }
