6caf9c2d3292b641efd15e50ee5c7523ee35e666
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 01:05:56 +0530

    Update Square.java

--- Assignment9/Factory for ADT/Square.java
+++ Assignment9/Factory for ADT/Square.java
@@ -3,58 +3,57 @@ package Assignment9;
 import java.util.Date;
 import java.util.List;
 public class Square implements Shape {
-	int side;
-	Point coordinates;
-	String timestamp;
-	int id;
-	
-	
-	
-	 //Constructor takes two arguments point object(Screen coordinates)
-	 //and list object(Parameter of shapes) 
-	 
-	public Square(Point point, List<Integer> list) {
-		coordinates = new Point(point.getX(), point.getY());
-		side = list.get(0);
-		timestamp = new Date().toString();
-	}
-	
-	//setters
-	@Override
-	public void setId(int shapeId) {
-		id = shapeId;
+   int side;
+   Point coordinates;
+   String timestamp;
+   int shapeId;
 		
-	}
-	//getters
-	@Override
-	public String getTimestamp() {
-		return timestamp;
-	}
-	@Override
-	public int getId() {
-		return id;
-	}
-	@Override
-	public Point getOrigin() {
-		return coordinates;
-	}
+  /* Constructor takes two arguments 
+     1. point object(Screen coordinates)
+     2. list object(Parameter of shapes) */ 
+	 
+     public Square(Point point, List<Integer> list) {
+	coordinates = new Point(point.getX(), point.getY());
+	side = list.get(0);
+	timestamp = new Date().toString();
+     }
 	
-	@Override
-	public boolean isPointEnclosed(Point point) {
-		if(point.getX() > coordinates.getX() &&	point.getX() < coordinates.getX() + side &&	point.getY() > coordinates.getY() && point.getY() < coordinates.getY() + side)
-				return true;
-			return false;
-	}
+     //setters
+     @Override
+     public void setId(int shapeId) {
+	this.shapeId = shapeId;	
+     }
 	
-	//return area of square
-	@Override
-	public double getArea() {
-		return side*side;
-	}
+     //getters
+     @Override
+     public String getTimestamp() {
+	return timestamp;
+     }
+     @Override
+     public int getId() {
+	return shapeId;
+     }
+     @Override
+     public Point getOrigin() {
+	return coordinates;
+     }
 	
-	//return parameter of square
-	@Override
-	public double getPerimeter() {
-		return 4*side;
-	}
+     @Override
+     public boolean isPointEnclosed(Point point) {
+	if(point.getX() > coordinates.getX() &&	point.getX() < coordinates.getX() + side && point.getY() > coordinates.getY() && point.getY() < coordinates.getY() + side)
+	   return true;
+	return false;
+     }
+	
+     //return area of square
+     @Override
+     public double getArea() {
+	return side * side;
+     }
+	
+     //return perimeter of square
+     @Override
+     public double getPerimeter() {
+	return 4 * side;
+     }
 }	
