64c0c151ef3837a7bb888a21d3468ebc02c8dfb3
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 01:30:50 +0530

    Update Parrot.java

--- Assignment8/Zoo_Management_system/Parrot.java
+++ Assignment8/Zoo_Management_system/Parrot.java
@@ -2,50 +2,50 @@ package Assignment8;
 
 class Parrot extends Bird {
 	
-	//Constructor initializing the animalName
-	Parrot(){
-		super();
-		animalName = "parrot";
-	}
+     //Constructor 
+     Parrot(){
+	super();
+	animalName = "parrot";
+     }
 	
-	//setters
-	@Override
-	void setId(int id) {
-		this.id = id;
-	}	
-	@Override
-	void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	void setAge(double age){
-		this.age = age;
-	}
-	@Override
-	void setWeight(double weight){
-		this.weight = weight;
-	}
+     //setters
+     @Override
+     void setId(int id) {
+	this.id = id;
+     }	
+     @Override
+     void setName(String name) {
+	this.name = name;
+     }
+     @Override
+     void setAge(double age){
+	this.age = age;
+     }
+     @Override
+     void setWeight(double weight){
+	this.weight = weight;
+     }
 		
-	//getters
-	@Override
-	String getName(){
-		return name;
-	}
-	@Override
-	String getType(){
-		return type;
-	}
-	@Override
-	String getAnimalName() {
-		return animalName;
-	}
-	@Override
-	int getId() {
-		return id;
-	}
-	@Override
-	String getSound() {
-		return "Chirp";
-	}
+     //getters
+     @Override
+     String getName(){
+	return name;
+     }
+     @Override
+     String getType(){
+	return type;
+     }
+     @Override
+     String getAnimalName() {
+	return animalName;
+     }
+     @Override
+     int getId() {
+	return id;
+     }
+     @Override
+     String getSound() {
+	return "Chirp";
+     }
 }
 
