0bcb0d0a5216ba35e5c4caf2efb7ba2f5b2e10b4
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 01:33:18 +0530

    Update Snake.java

--- Assignment8/Zoo_Management_system/Snake.java
+++ Assignment8/Zoo_Management_system/Snake.java
@@ -2,49 +2,49 @@ package Assignment8;
 
 class Snake extends Reptile {
 	
-	//Constructor initializing the animalName
-	Snake(){
-		super();
-		animalName = "sanke";
-	}
+	//Constructor
+     Snake(){
+	super();
+	animalName = "sanke";
+     }
 
-	//setters
-	@Override
-	void setId(int id) {
-		this.id = id;
-	}	
-	@Override
-	void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	void setAge(double age){
-		this.age = age;
-	}
-	@Override
-	void setWeight(double weight){
-		this.weight = weight;
-	}
+     //setters
+     @Override
+     void setId(int id) {
+	this.id = id;
+     }	
+     @Override
+     void setName(String name) {
+	this.name = name;
+     }
+     @Override
+     void setAge(double age){
+	this.age = age;
+     }
+     @Override
+     void setWeight(double weight){
+	this.weight = weight;
+     }
 	
-	//getters
-	@Override
-	String getName(){
-		return name;
-	}
-	@Override
-	String getType(){
-		return type;
-	}
-	@Override
-	String getAnimalName() {
-		return animalName;
-	}
-	@Override
-	int getId() {
-		return id;
-	}
-	@Override
-	String getSound() {
-		return "Hiss";
-	}
+     //getters
+     @Override
+     String getName(){
+	return name;
+     }
+     @Override
+     String getType(){
+	return type;
+     }
+     @Override
+     String getAnimalName() {
+	return animalName;
+     }
+     @Override
+     int getId() {
+	return id;
+     }
+     @Override
+     String getSound() {
+	return "Hiss";
+     }
 }
