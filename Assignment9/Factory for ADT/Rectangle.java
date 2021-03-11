e4da9134c7f81cebf69fe2511e7be4d4170b137f
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 00:40:59 +0530

    Update Rectangle.java

--- Assignment9/Factory for ADT/Rectangle.java
+++ Assignment9/Factory for ADT/Rectangle.java
@@ -3,58 +3,57 @@ package Assignment9;
 import java.util.Date;
 import java.util.List;
 public class Rectangle implements Shape {
-	int width, height;
-	Point coordinates;
-	String timestamp;
-	int id;
+   int width, height;
+   Point coordinates;
+   String timestamp;
+   int shapeId;
 	
+  /*Constructor takes two argument 
+    1. point object(Screen coordinates)
+    2. list object(Parameter of shapes) */ 
+   public Rectangle(Point point, List<Integer> list) {
+      coordinates = new Point(point.getX(), point.getY());
+      width = list.get(0);
+      height = list.get(1);
+      timestamp = new Date().toString();
+   }
 	
-	  //Constructor takes two argument point object(Screen coordinates)
-	  //and list object(Parameter of shapes) 
-	public Rectangle(Point point, List<Integer> list) {
-		coordinates = new Point(point.getX(), point.getY());
-		width = list.get(0);
-		height = list.get(1);
-		timestamp = new Date().toString();
-	}
+   //setters
+   @Override
+   public void setId(int shapeId) {
+      this.shapeId = shapeId;	
+   }
 	
-	//setters
-	@Override
-	public void setId(int shapeId) {
-		id = shapeId;
-		
-	}
+   //getters
+   @Override
+   public Point getOrigin() {
+      return coordinates;
+   }
+   @Override
+   public String getTimestamp() {
+      return timestamp;
+   }
+   @Override
+   public int getId() {
+      return shapeId;
+   }
 	
-	//getters
-	@Override
-	public Point getOrigin() {
-		return coordinates;
-	}
-	@Override
-	public String getTimestamp() {
-		return timestamp;
-	}
-	@Override
-	public int getId() {
-		return id;
-	}
+   //return area of rectangle 
+   @Override
+   public double getArea() {
+      return width * height;
+   }
 	
-	//return area of rectangle 
-	@Override
-	public double getArea() {
-		return width * height;
-	}
-	
-	//return parameter of rectangle 
-	@Override
-	public double getPerimeter() {
-		return 2*(width+height);
-	}
+   //return parameter of rectangle 
+   @Override
+   public double getPerimeter() {
+      return 2*(width+height);
+   }
 
-	@Override
-	public boolean isPointEnclosed(Point point) {
-		if(point.getX() > coordinates.getX() &&	point.getX() < coordinates.getX() + width && point.getY() > coordinates.getY() && point.getY() < coordinates.getY() + height)
-			return true;
-		return false;
-	}
+   @Override
+   public boolean isPointEnclosed(Point point) {
+      if(point.getX() > coordinates.getX() &&	point.getX() < coordinates.getX() + width && point.getY() > coordinates.getY() && point.getY() < coordinates.getY() + height)
+	return true;
+      return false;
+   }
 }
