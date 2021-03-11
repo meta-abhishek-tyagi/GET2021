e7101661a0869d313fc910653902eb6e6750deb5
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 00:36:31 +0530

    Update Point.java

--- Assignment9/Factory for ADT/Point.java
+++ Assignment9/Factory for ADT/Point.java
@@ -4,17 +4,17 @@ public class Point {
     private int xCoordinate;
     private int yCoordinate;
 
-	//Constructor
-	Point(int xCoordinate, int yCoordinate){
-		this.xCoordinate = xCoordinate;
-		this.yCoordinate = yCoordinate;
-	}
+    //Constructor
+    Point(int xCoordinate, int yCoordinate){
+	this.xCoordinate = xCoordinate;
+	this.yCoordinate = yCoordinate;
+    }
 	
-	//getters
-	int getX(){
-		return xCoordinate;
-	}
-	int getY(){
-		return yCoordinate;
-	}
+    //getters
+    int getX(){
+	return xCoordinate;
+    }
+    int getY(){
+	return yCoordinate;
+    }
 }
