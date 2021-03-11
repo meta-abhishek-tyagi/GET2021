7549bb4bfa8866430b6637472cd38fbfd4938848
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 01:20:43 +0530

    Update Lion.java

--- Assignment8/Zoo_Management_system/Lion.java
+++ Assignment8/Zoo_Management_system/Lion.java
@@ -2,49 +2,49 @@ package Assignment8;
 
 class Lion extends Mammal {
 	
-	//Constructor initializing the animalName
-	Lion(){
-		super();
-		animalName = "lion";
-	}
+    //Constructor 
+    Lion(){
+	super();
+	animalName = "lion";
+    }
 	
-	//setters
-	@Override
-	void setId(int id) {
-		this.id = id;
-	}	
-	@Override
-	void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	void setAge(double age){
-		this.age = age;
-	}
-	@Override
-	void setWeight(double weight){
-		this.weight = weight;
-	}
+    //setters
+    @Override
+    void setId(int id) {
+	this.id = id;
+    }	
+    @Override
+    void setName(String name) {
+	this.name = name;
+    }
+    @Override
+    void setAge(double age){
+	this.age = age;
+    }
+    @Override
+    void setWeight(double weight){
+	this.weight = weight;
+    }
 	
-	//getters
-	@Override
-	String getName(){
-		return name;
-	}
-	@Override
-	String getType(){
-		return type;
-	}
-	@Override
-	String getAnimalName() {
-		return animalName;
-	}
-	@Override
-	int getId() {
-		return id;
-	}
-	@Override
-	String getSound() {
-		return "Roar";
-	}
+    //getters
+    @Override
+    String getName(){
+	return name;
+    }
+    @Override
+    String getType(){
+	return type;
+    }
+    @Override
+    String getAnimalName() {
+	return animalName;
+    }
+    @Override
+    int getId() {
+	return id;
+    }
+    @Override
+    String getSound() {
+	return "Roar";
+    }
 }
