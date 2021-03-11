b0b4f79d9361a6408ac9bf4b860ff50fced4e165
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-12 01:14:55 +0530

    Update Animal.java

--- Assignment8/Zoo_Management_system/Animal.java
+++ Assignment8/Zoo_Management_system/Animal.java
@@ -9,15 +9,32 @@ abstract class Animal {
      protected int id;
      protected String name;
 
-     abstract String getSound();  //return the sound of animal
-     abstract String getAnimalName(); //return the name of the animal like lion,snake etc
-     abstract String getType(); //return the type of the animal like mammal,reptile etc.
-     abstract String getName(); //return the name of a animal like there will be a name of lion,snake
-     abstract void setName(String nameInp); //set the name of the animal ,will take string as arg.
-     abstract void setAge(double age); //set the age of the animal ,will take double as argument
-     abstract void setWeight(double weight); //set the weight of the animal , will take double value
-     abstract void setId(int animalId); //set the id of a animal ,will take int argument
-     abstract int getId(); //return the id of the animal
+     //return the sound of the animal
+     abstract String getSound(); 
+     
+     //return the name of the animal like lion, snake, etc.
+     abstract String getAnimalName(); 
+     
+     //return the type of the animal like mammal,reptile etc.
+     abstract String getType(); 
+     
+     //return the name of a animal like there will be a name of lion is sheru
+     abstract String getName(); 
+     
+     //set the name of the animal like for lion name will be sheru
+     abstract void setName(String nameInp); 
+     
+     //set the age of the animal 
+     abstract void setAge(double age); 
+     
+     //set the weight of the animal 
+     abstract void setWeight(double weight);
+     
+     //set the id of a animal 
+     abstract void setId(int animalId); 
+     
+     //return the id of the animal
+     abstract int getId(); 
 }
 
 
