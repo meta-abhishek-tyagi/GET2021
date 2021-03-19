6866399435711bbd15f9edd3fafb2a7db0719225
meta-abhishek-tyagi <79212766+meta-abhishek-tyagi@users.noreply.github.com>
2021-03-19 17:25:01 +0530

    Update Employee.java

--- DSA_Assignment4/Employee.java
+++ DSA_Assignment4/Employee.java
@@ -31,17 +31,17 @@ class Employee{
 	//method to sort naturally 
 	public static void naturalSort(){
 		ArrayList<Integer> sortedKeys = new ArrayList<>(map.keySet()); 
-    Collections.sort(sortedKeys);  
-    System.out.println("\nEmpId \t      EmpName \t        EmpAddress \t");
-    for(Integer id : sortedKeys) {
-       System.out.println(map.get(id).employeeId + " \t \t " + map.get(id).name + "\t\t" + map.get(id).address);
-    }     
+                Collections.sort(sortedKeys);  
+                System.out.println("\nEmpId \t      EmpName \t        EmpAddress \t");
+                for(Integer id : sortedKeys) {
+                      System.out.println(map.get(id).employeeId + " \t \t " + map.get(id).name + "\t\t" + map.get(id).address);
+                }     
 	}
 	
 	//To print HashMap sorted by name
 	public static void print(List<Map.Entry<Integer, Employee>> entryList){
 		Iterator<Map.Entry<Integer, Employee>> itr = entryList.iterator();
-    System.out.println("EmpId \t      EmpName \t        EmpAddress \t");
+                System.out.println("EmpId \t      EmpName \t        EmpAddress \t");
 		while(itr.hasNext()){
 			Map.Entry<Integer, Employee> entry=itr.next();
 			System.out.println(entry.getKey() + " \t \t " + entry.getValue().name + "\t\t"+entry.getValue().address);
@@ -49,12 +49,12 @@ class Employee{
 	}
    
 	//To print List
-    public static void printList(){
-    	System.out.println("EmpId \t      EmpName \t        EmpAddress \t");
-    	for(Employee emp : empList){
-    		System.out.println("  "+emp.getId() + " \t \t " + emp.getName()+"\t\t" + emp.getAddress() + " \t ");
-    	}
-    }
+        public static void printList(){
+    	        System.out.println("EmpId \t      EmpName \t        EmpAddress \t");
+          	for(Employee emp : empList){
+    	        	System.out.println("  "+emp.getId() + " \t \t " + emp.getName()+"\t\t" + emp.getAddress() + " \t ");
+    	        }
+        }
 
 	public static void main(String args[]){
 		Employee emp1=new Employee("A", 1, "Ram nagar");
@@ -83,11 +83,11 @@ class Employee{
 		
 		System.out.println("\n \n After Sorting by Name......\n");
 		List<Map.Entry<Integer, Employee>> entryList = new ArrayList<Map.Entry<Integer, Employee>>(map.entrySet());
-    Collections.sort(entryList, new Comparator<Map.Entry<Integer, Employee>>() {
-           public int compare(Map.Entry<Integer, Employee> integerEmployeeEntry, Map.Entry<Integer, Employee> integerEmployeeEntry2) {
-                return integerEmployeeEntry.getValue().name.compareTo(integerEmployeeEntry2.getValue().name);
-           }
-    });
-    print(entryList);    
+                Collections.sort(entryList, new Comparator<Map.Entry<Integer, Employee>>() {          
+	               public int compare(Map.Entry<Integer, Employee> integerEmployeeEntry, Map.Entry<Integer, Employee> integerEmployeeEntry2) {
+                             return integerEmployeeEntry.getValue().name.compareTo(integerEmployeeEntry2.getValue().name);
+                       }
+                });
+                print(entryList);    
 	}	
 }
